cbdea7e8059c10376772c2a993bb468309f9bf6e
段昊辰 <duanhaochen@126.com>
2016-09-16 21:39:49 +0800

    Update noWebGL.js

--- js/index/noWebGL.js
+++ js/index/noWebGL.js
@@ -5,7 +5,7 @@
     var taiyangxi = document.getElementById("taiyangxi");
     if (taiyangxi.getElementsByTagName("canvas").length === 0) {
         // 没有canvas执行该事件
-        taiyangxi.style.cssText = "background-image: url(\"/img/yuzhoubg" + Math.floor(Math.random() * 3) + ".jpg\")";
+        taiyangxi.style.cssText = "background-image: url(\"/img/index/yuzhoubg" + Math.floor(Math.random() * 3) + ".jpg\")";
         alert("您的浏览器不支持WebGL。");
     }
 })();
